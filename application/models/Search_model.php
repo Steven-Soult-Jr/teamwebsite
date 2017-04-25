154b025c24ce1a6654dcdd96f05228618998b1bb
jonasvinter-jensen <jonasvinter-jensen@jonass-mac-mini>
2017-04-24 17:57:56 -0700

    Updated header based on M3 feedback (moved searchbar up, added a condition dropdown and changed design of sell button).

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -1,32 +1,25 @@
 <?php
+
 class Search_model extends CI_Model {
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->load->database();
     }
 
-
-    public function get_items($keyword, $category){
+    public function get_items($keyword, $category) {
         $keyword = trim($keyword);
 
         //searches the database's 'items' table for similarity in 'name' and 'category' columns
-        if($category=="All")
-        {
+        if ($category == "All") {
             $this->db->select('*');
-        }
-        else if($category=="Other")
-        {
-            $category=NULL; //If category was set to Other, choose any database item with no category 
+        } else if ($category == "Other") {
+            $category = NULL; //If category was set to Other, choose any database item with no category 
             $this->db->where('category', $category);
-        }
-        else 
-        {
+        } else {
             $this->db->where('category', $category);
         }
         $this->db->from('search_test');
-        if($keyword != "")
-        {
+        if ($keyword != "") {
             $this->db->like('name', $keyword);
             $this->db->or_like('category', $keyword);
         }
@@ -35,7 +28,6 @@ class Search_model extends CI_Model {
 
         //returns query results in an array
         return $query->result_array();
-
     }
 
 }
