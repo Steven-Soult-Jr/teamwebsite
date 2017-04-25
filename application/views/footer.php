8bdc262d1a2687495b1fd7f0e1e0850038b0ef12
jonasvinter-jensen <jonasvinter-jensen@jonass-mac-mini>
2017-04-24 17:54:33 -0700

    Updated header based on M3 feedback (moved searchbar up, added a condition dropdown and changed design of sell button).

--- application/views/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- Footer contains the legal information, and the closing tags for the file. It will be loaded by the controller as
-     the bottom part of each page. -->
-<footer class="container-fluid bg-4 text-center">
-    <p>Not an actual site - SFSU CSC 648-2 Team 12 Class Project</p>
-</footer>
-
-</body>
-</html>
